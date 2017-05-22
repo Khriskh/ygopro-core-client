15f02f8a0d72bf25e6c1b9829cf659f64fd62607
Khriskh <chrisqs16@hotmail.com>
2017-05-22 17:10:01 -0500

    7.5

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -1,23 +1,17 @@
 #include "config.h"
 #include "game.h"
-//#include "image_manager.h"
+#include "image_manager.h"
 #include "data_manager.h"
 #include "deck_manager.h"
 #include "replay.h"
-//#include "materials.h"
-//#include "duelclient.h"
+#include "materials.h"
+#include "duelclient.h"
 #include "netserver.h"
-//#include "single_mode.h"
-
-#ifdef _WIN32
-#define strcasecmp _stricmp
-#include "dirent.h"
-#endif // _WIN32
+#include "single_mode.h"
 
 #ifndef _WIN32
 #include <sys/types.h>
 #include <dirent.h>
-#include <unistd.h>
 #endif
 
 const unsigned short PRO_VERSION = 0x2337;
@@ -26,54 +20,6 @@ namespace ygo {
 
 Game* mainGame;
 
-unsigned short aServerPort;
-unsigned int lflist;
-unsigned char rule;
-unsigned char mode;
-unsigned char duel_rule;
-bool no_check_deck;
-bool no_shuffle_deck;
-unsigned int start_lp;
-unsigned short time_limit;
-unsigned short replay_mode;
-unsigned char start_hand;
-unsigned char draw_count;
-
-void Game::MainServerLoop(int bDuel_mode, int lflist) {
-	deckManager.LoadLFList();
-	dataManager.LoadDB("cards.cdb");
-	
-	//load expansions
-	DIR * dir;
-	struct dirent * dirp;
-	const char *foldername = "./expansions/";
-	if((dir = opendir(foldername)) != NULL) {
-		while((dirp = readdir(dir)) != NULL) {
-			size_t len = strlen(dirp->d_name);
-			if(len < 5 || strcasecmp(dirp->d_name + len - 4, ".cdb") != 0)
-				continue;
-			char *filepath = (char *)malloc(sizeof(char)*(len + strlen(foldername)));
-			strncpy(filepath, foldername, strlen(foldername)+1);
-			strncat(filepath, dirp->d_name, len);
-			dataManager.LoadDB(filepath);
-			free(filepath);
-		}
-		closedir(dir);
-	}
-	
-	aServerPort = NetServer::StartServer(aServerPort);
-	NetServer::Initduel(bDuel_mode, lflist);
-	printf("%u\n", aServerPort);
-	fflush(stdout);
-	while(NetServer::net_evbase) {
-#ifdef WIN32
-		Sleep(200);
-#else
-		usleep(200000);
-#endif
-	}
-}
-/*
 bool Game::Initialize() {
 	srand(time(0));
 	LoadConfig();
@@ -140,8 +86,8 @@ bool Game::Initialize() {
 	SetWindowsIcon();
 	//main menu
 	wchar_t strbuf[256];
-	myswprintf(strbuf, L"YGOPro Version:%X.0%X.%X", PRO_VERSION >> 12, (PRO_VERSION >> 4) & 0xff, PRO_VERSION & 0xf);
-	wMainMenu = env->addWindow(rect<s32>(370, 200, 650, 415), false, strbuf);
+	myswprintf(strbuf, L"YGOPro 233 Test Version:%X.%X.%X", PRO_VERSION >> 12, (PRO_VERSION >> 4) & 0xff, PRO_VERSION & 0xf);
+	wMainMenu = env->addWindow(rect<s32>(370, 200, 950, 600), false, strbuf);
 	wMainMenu->getCloseButton()->setVisible(false);
 	btnLanMode = env->addButton(rect<s32>(10, 30, 270, 60), wMainMenu, BUTTON_LAN_MODE, dataManager.GetSysString(1200));
 	btnServerMode = env->addButton(rect<s32>(10, 65, 270, 95), wMainMenu, BUTTON_SINGLE_MODE, dataManager.GetSysString(1201));
@@ -149,6 +95,26 @@ bool Game::Initialize() {
 //	btnTestMode = env->addButton(rect<s32>(10, 135, 270, 165), wMainMenu, BUTTON_TEST_MODE, dataManager.GetSysString(1203));
 	btnDeckEdit = env->addButton(rect<s32>(10, 135, 270, 165), wMainMenu, BUTTON_DECK_EDIT, dataManager.GetSysString(1204));
 	btnModeExit = env->addButton(rect<s32>(10, 170, 270, 200), wMainMenu, BUTTON_MODE_EXIT, dataManager.GetSysString(1210));
+
+	env->addStaticText(L"此版本为23333服内测版本，", rect<s32>(10, 220, 270, 240), false, false, wMainMenu);
+	env->addStaticText(L"遇到问题请务必反馈，群：275986039。", rect<s32>(10, 240, 270, 260), false, false, wMainMenu);
+	env->addStaticText(L"不要觉得会有别人报告，别人也会这么想。", rect<s32>(10, 260, 270, 280), false, false, wMainMenu);
+
+	env->addStaticText(L"此版本会不定期更新，", rect<s32>(10, 300, 270, 320), false, false, wMainMenu);
+	env->addStaticText(L"建议随时在233服官网下载最新版本！", rect<s32>(10, 320, 270, 340), false, false, wMainMenu);
+
+	env->addStaticText(L"网址：", rect<s32>(10, 340, 270, 360), false, false, wMainMenu);
+	env->addStaticText(L"http://mercury233.me/ygosrv233/", rect<s32>(10, 360, 270, 380), false, false, wMainMenu);
+
+	env->addStaticText(L"已知问题：\n\n\
+代替破坏的效果可以用不会被破坏的卡\n\
+代替破坏。\n\n\
+装弹枪管龙、娱乐伙伴 异色眼同调士、\n\
+淘气仙星灯光舞台、王车连接的处理存\n\
+在问题。\n\n\n\n\n\
+从额外卡组特殊召唤的操作可视为全部\n\
+更新完成，如有遗漏请务必反馈。", rect<s32>(300, 30, 550, 390), false, true, wMainMenu);
+
 	//lan mode
 	wLanWindow = env->addWindow(rect<s32>(220, 100, 800, 520), false, dataManager.GetSysString(1200));
 	wLanWindow->getCloseButton()->setVisible(false);
@@ -262,19 +228,19 @@ bool Game::Initialize() {
 	btnDP->setEnabled(false);
 	btnDP->setPressed(true);
 	btnDP->setVisible(false);
-	btnSP = env->addButton(rect<s32>(65, 0, 115, 20), wPhase, -1, L"\xff33\xff30");
+	btnSP = env->addButton(rect<s32>(0, 0, 50, 20), wPhase, -1, L"\xff33\xff30");
 	btnSP->setEnabled(false);
 	btnSP->setPressed(true);
 	btnSP->setVisible(false);
-	btnM1 = env->addButton(rect<s32>(130, 0, 180, 20), wPhase, -1, L"\xff2d\xff11");
+	btnM1 = env->addButton(rect<s32>(160, 0, 210, 20), wPhase, -1, L"\xff2d\xff11");
 	btnM1->setEnabled(false);
 	btnM1->setPressed(true);
 	btnM1->setVisible(false);
-	btnBP = env->addButton(rect<s32>(195, 0, 245, 20), wPhase, BUTTON_BP, L"\xff22\xff30");
+	btnBP = env->addButton(rect<s32>(160, 0, 210, 20), wPhase, BUTTON_BP, L"\xff22\xff30");
 	btnBP->setVisible(false);
-	btnM2 = env->addButton(rect<s32>(260, 0, 310, 20), wPhase, BUTTON_M2, L"\xff2d\xff12");
+	btnM2 = env->addButton(rect<s32>(160, 0, 210, 20), wPhase, BUTTON_M2, L"\xff2d\xff12");
 	btnM2->setVisible(false);
-	btnEP = env->addButton(rect<s32>(325, 0, 375, 20), wPhase, BUTTON_EP, L"\xff25\xff30");
+	btnEP = env->addButton(rect<s32>(320, 0, 370, 20), wPhase, BUTTON_EP, L"\xff25\xff30");
 	btnEP->setVisible(false);
 	btnShuffle = env->addButton(rect<s32>(0, 0, 50, 20), wPhase, BUTTON_CMD_SHUFFLE, dataManager.GetSysString(1307));
 	btnShuffle->setVisible(false);
@@ -659,8 +625,6 @@ bool Game::Initialize() {
 	hideChatTimer = 0;
 	return true;
 }
-*/
-/*
 void Game::MainLoop() {
 	wchar_t cap[256];
 	camera = smgr->addCameraSceneNode(0);
@@ -753,8 +717,6 @@ void Game::MainLoop() {
 	SaveConfig();
 //	device->drop();
 }
-*/
-/*
 void Game::BuildProjectionMatrix(irr::core::matrix4& mProjection, f32 left, f32 right, f32 bottom, f32 top, f32 znear, f32 zfar) {
 	for(int i = 0; i < 16; ++i)
 		mProjection[i] = 0;
@@ -972,7 +934,7 @@ void Game::LoadConfig() {
 	gameConf.control_mode = 0;
 	gameConf.draw_field_spell = 1;
 	gameConf.separate_clear_button = 1;
-	gameConf.auto_search_limit = -1;
+	gameConf.auto_search_limit = 0;
 	fseek(fp, 0, SEEK_END);
 	int fsize = ftell(fp);
 	fseek(fp, 0, SEEK_SET);
@@ -1277,14 +1239,12 @@ void Game::CloseDuelWindow() {
 	ClearTextures();
 	closeDoneSignal.Set();
 }
-*/
 int Game::LocalPlayer(int player) {
 	return dInfo.isFirst ? player : 1 - player;
 }
 const wchar_t* Game::LocalName(int local_player) {
 	return local_player == 0 ? dInfo.hostname : dInfo.clientname;
 }
-/*
 void Game::SetWindowsIcon() {
 #ifdef _WIN32
 	HINSTANCE hInstance = (HINSTANCE)GetModuleHandleW(NULL);
@@ -1305,6 +1265,5 @@ void Game::FlashWindow() {
 	FlashWindowEx(&fi);
 #endif
 }
-*/
 
 }
