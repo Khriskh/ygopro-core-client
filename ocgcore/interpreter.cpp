4cccdd6e10f035c48e30190cba00a63232e55019
Khriskh <chrisqs16@hotmail.com>
2017-10-05 17:03:28 -0500

    cliet mercury

--- ocgcore/interpreter.cpp
+++ ocgcore/interpreter.cpp
@@ -603,7 +603,6 @@ interpreter::interpreter(duel* pd): coroutines(256) {
 	//extra scripts
 	load_script((char*) "./script/constant.lua");
 	load_script((char*) "./script/utility.lua");
-	load_script((char*) "./specials/special.lua");
 }
 interpreter::~interpreter() {
 	lua_close(lua_state);
@@ -716,17 +715,14 @@ int32 interpreter::load_card_script(uint32 code) {
 		lua_pushstring(current_state, "__index");
 		lua_pushvalue(current_state, -2);
 		lua_rawset(current_state, -3);
-		//load special and extra scripts first
-		sprintf(script_name, "./specials/c%d.lua", code);
+		//load extra scripts
+		sprintf(script_name, "./expansions/script/c%d.lua", code);
 		if (!load_script(script_name)) {
-			sprintf(script_name, "./expansions/script/c%d.lua", code);
-			if (!load_script(script_name)) {
-				sprintf(script_name, "./script/c%d.lua", code);
-				if (!load_script(script_name)) {
-					return OPERATION_FAIL;
-				}
-			}
-		}
+			sprintf(script_name, "./script/c%d.lua", code);
+	 		if (!load_script(script_name)) {
+	 			return OPERATION_FAIL;
+ 			}
+  		}
 	}
 	return OPERATION_SUCCESS;
 }
