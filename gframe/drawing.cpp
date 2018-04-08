c325e305a78166de4ed781dfd4b44f6b4dfcd322
khriskh <khriskh@gmail.com>
2018-04-07 21:35:28 -0500

    fix

--- gframe/drawing.cpp
+++ gframe/drawing.cpp
@@ -566,8 +566,8 @@ void Game::DrawMisc() {
 	//DrawShadowText(numFont, dInfo.strLP[1], Resize(691, 12, 992, 30), Resize(0, 1, 2, 0), 0xffffff00, 0xff000000, true, false, 0);
 	
 	//name
-	recti p1size = Resize(420, 57, 629, 50);
-	recti p2size = Resize(900, 57, 986, 50);
+	recti p1size = Resize(420, 54, 629, 50);
+	recti p2size = Resize(900, 54, 986, 50);
 	//recti p1size = Resize(335, 31, 629, 50);
 	//recti p2size = Resize(986, 31, 986, 50);
 	if(!dInfo.isTag || !dInfo.tag_player[0])
