e735931307f8253f57a0c25bbf506169db1bdf49
khriskh <chrisqs16@hotmail.com>
2018-06-04 20:10:49 -0500

    fix5

--- ocgcore/premake4.lua
+++ ocgcore/premake4.lua
@@ -7,6 +7,3 @@ project "ocgcore"
 	
 	configuration "windows"
 		includedirs { ".." }
-	configuration "not windows"
-		buildoptions { "-std=c++11", "-Wno-unused-parameter" }
-		includedirs { "/usr/include/lua", "/usr/include/lua5.3", "/usr/include/lua/5.3" }
