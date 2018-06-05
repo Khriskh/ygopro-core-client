11aa39b74033f30d42e2009776b569680366dd03
khriskh <chrisqs16@hotmail.com>
2018-06-04 20:50:33 -0500

    fix9

--- gframe/premake4.lua
+++ gframe/premake4.lua
@@ -6,21 +6,18 @@ project "ygopro"
     files { "**.cpp", "**.cc", "**.c", "**.h" }
     excludes "lzma/**"
     includedirs { "../ocgcore" }
-    links { "ocgcore", "clzma", "Irrlicht", "freetype", "sqlite3", "lua" , "event" }
-
-    if os.getenv("YGOPRO_COMPAT_MYCARD") then
-        defines { "YGOPRO_COMPAT_MYCARD" }
-    end
+    links { "ocgcore", "clzma", "Irrlicht", "freetype", "sqlite3" , "event", "irrKlang"}
 
     configuration "windows"
         files "ygopro.rc"
         excludes "CGUIButton.cpp"
-        includedirs { "../irrlicht/include", "../freetype/include", "../event/include", "../sqlite3" }
+        includedirs { "../irrlicht/include", "../freetype/include", "../event/include", "../sqlite3", "../irrKlang-1.5.0/include" }
+ 		libdirs { "../irrKlang-1.5.0/lib/Win32-visualStudio" }
         links { "opengl32", "ws2_32", "winmm", "gdi32", "kernel32", "user32", "imm32" }
     configuration {"windows", "not vs*"}
         includedirs { "/mingw/include/irrlicht", "/mingw/include/freetype2" }
     configuration "not vs*"
-        buildoptions { "-std=gnu++0x", "-fno-rtti" }
+        buildoptions { "-std=gnu++0x", "-fno-rtti", "-fpermissive" }
     configuration "not windows"
         includedirs { "/usr/include/lua", "/usr/include/lua5.3", "/usr/include/lua/5.3", "/usr/include/irrlicht", "/usr/include/freetype2" }
         excludes { "COSOperator.*" }
