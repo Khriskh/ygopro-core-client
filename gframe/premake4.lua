f024e9416d86fe9c7065e31fed89f9b45d1dc633
khriskh <chrisqs16@hotmail.com>
2018-06-04 20:44:56 -0500

    fix8

--- gframe/premake4.lua
+++ gframe/premake4.lua
@@ -7,14 +7,7 @@ project "ygopro"
     excludes "lzma/**"
     includedirs { "../ocgcore" }
     links { "ocgcore", "clzma", "Irrlicht", "freetype", "sqlite3", "lua" , "event" }
-    if USE_IRRKLANG then
-        defines { "YGOPRO_USE_IRRKLANG" }
-        links { "irrKlang", "ikpmp3" }
-        includedirs { "../irrklang/include" }
-        if IRRKLANG_PRO then
-            defines { "IRRKLANG_STATIC" }
-        end
-    end
+
     if os.getenv("YGOPRO_COMPAT_MYCARD") then
         defines { "YGOPRO_COMPAT_MYCARD" }
     end
@@ -23,9 +16,6 @@ project "ygopro"
         files "ygopro.rc"
         excludes "CGUIButton.cpp"
         includedirs { "../irrlicht/include", "../freetype/include", "../event/include", "../sqlite3" }
-        if USE_IRRKLANG then
-            libdirs { "../irrklang/lib/Win32-visualStudio" }
-        end
         links { "opengl32", "ws2_32", "winmm", "gdi32", "kernel32", "user32", "imm32" }
     configuration {"windows", "not vs*"}
         includedirs { "/mingw/include/irrlicht", "/mingw/include/freetype2" }
