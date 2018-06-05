be8d8c30ca56b311efc13659ee1f03c4715e9249
khriskh <chrisqs16@hotmail.com>
2018-06-04 20:24:08 -0500

    fix6

--- gframe/premake4.lua
+++ gframe/premake4.lua
@@ -14,7 +14,3 @@ project "ygopro"
         includedirs { "../irrlicht/include", "../freetype/include", "../event/include", "../sqlite3", "../irrKlang-1.5.0/include" }
  		libdirs { "../irrKlang-1.5.0/lib/Win32-visualStudio" }
         links { "opengl32", "ws2_32", "winmm", "gdi32", "kernel32", "user32", "imm32" }
-    configuration {"windows", "not vs*"}
-        includedirs { "/mingw/include/irrlicht", "/mingw/include/freetype2" }
-    configuration "not vs*"
-        buildoptions { "-std=gnu++0x", "-fno-rtti", "-fpermissive" }
--- ocgcore/premake4.lua
+++ ocgcore/premake4.lua
@@ -2,8 +2,5 @@ project "ocgcore"
 	kind "StaticLib"
 
 	files { "**.cc", "**.cpp", "**.c", "**.hh", "**.hpp", "**.h" }
-    links { "lua" }
-	buildoptions { "-Wall", "-Wextra", "-pedantic" }
-	
 	configuration "windows"
 		includedirs { ".." }
