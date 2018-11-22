85497b52cd170687d6b3d2eae6b96ca026516f41
ygo001 <ygoes.001@gmail.com>
2018-11-22 11:56:33 -0500

    ocgcore

--- ocgcore/libduel.cpp
+++ ocgcore/libduel.cpp
@@ -1256,7 +1256,7 @@ int32 scriptlib::duel_is_environment(lua_State *L) {
 			}
 		}
 		if(playerid == 1 || playerid == PLAYER_ALL) {
-			for(auto& pcard : pduel->game_field->player[0].list_szone) {
+			for(auto& pcard : pduel->game_field->player[1].list_szone) {
 				if(pcard && pcard->is_position(POS_FACEUP) && pcard->get_status(STATUS_EFFECT_ENABLED) && code == pcard->get_code())
 					ret = 1;
 			}
@@ -1270,7 +1270,7 @@ int32 scriptlib::duel_is_environment(lua_State *L) {
 			}
 		}
 		if(playerid == 1 || playerid == PLAYER_ALL) {
-			for(auto& pcard : pduel->game_field->player[0].list_mzone) {
+			for(auto& pcard : pduel->game_field->player[1].list_mzone) {
 				if(pcard && pcard->is_position(POS_FACEUP) && pcard->get_status(STATUS_EFFECT_ENABLED) && code == pcard->get_code())
 					ret = 1;
 			}
@@ -1320,7 +1320,7 @@ int32 scriptlib::duel_damage(lua_State *L) {
 	uint32 playerid = lua_tointeger(L, 1);
 	if(playerid != 0 && playerid != 1)
 		return 0;
-	int32 amount = lua_tointeger(L, 2);
+	int32 amount = std::round(lua_tonumber(L, 2));
 	if(amount < 0)
 		amount = 0;
 	uint32 reason = lua_tointeger(L, 3);
@@ -1338,7 +1338,7 @@ int32 scriptlib::duel_recover(lua_State *L) {
 	uint32 playerid = lua_tointeger(L, 1);
 	if(playerid != 0 && playerid != 1)
 		return 0;
-	int32 amount = lua_tointeger(L, 2);
+	int32 amount = std::round(lua_tonumber(L, 2));
 	if(amount < 0)
 		amount = 0;
 	uint32 reason = lua_tointeger(L, 3);
