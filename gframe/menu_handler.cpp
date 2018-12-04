470d893146cbdd9f19840e8375c926fba39a301d
Khriskh <chrisqs16@hotmail.com>
2018-12-04 17:52:09 -0500

    10

--- gframe/menu_handler.cpp
+++ gframe/menu_handler.cpp
@@ -134,7 +134,7 @@ bool MenuHandler::OnEvent(const irr::SEvent& event) {
 			}
 			case BUTTON_HOST_ONLINE: {
 				//mainGame->ebJoinPass->setText(L"Error occurs.");
-				mainGame->ebJoinPass->setText("LP"+mainGame->ebStartLP->getText());
+				mainGame->ebJoinPass->setText("LP" & mainGame->ebStartLP->getText());
 				mainGame->btnCreateHost->setEnabled(true);
 				mainGame->btnJoinHost->setEnabled(true);
 				mainGame->btnJoinCancel->setEnabled(true);
