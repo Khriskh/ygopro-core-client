16585c25fe474b71ddaa1dbfcf5989af63eb541a
Khriskh <chrisqs16@hotmail.com>
2020-03-31 16:21:33 -0500

    test 1.1

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -895,7 +895,7 @@ bool Game::Initialize() {
 	btnCancelOrFinish->setVisible(false);
 	//leave/surrender/exit
 	btnLeaveGame = env->addButton(rect<s32>(205, 5, 295, 80), 0, BUTTON_LEAVE_GAME, L"");
-	btnLeaveGame->setBackgroundColor(0xc00000FF);
+	btnLeaveGame->setUseAlphaChannel(true);
 	btnLeaveGame->setVisible(false);
 	//tip
 	stTip = env->addStaticText(L"", rect<s32>(0, 0, 150, 150), false, true, 0, -1, true);
